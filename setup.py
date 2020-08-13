1ab8eb66c1f3a887cfd4960b9c95acd73425a82f
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-08-13 10:59:32 +0100

    update travis testing

--- setup.py
+++ setup.py
@@ -11,15 +11,10 @@ def read(fname):
     return codecs.open(file_path, encoding="utf-8").read()
 
 
-# Add your dependencies in requirements.txt
-# Note: you can add test-specific requirements in tox.ini
-requirements = []
-with open("requirements.txt") as f:
-    for line in f:
-        stripped = line.split("#")[0].strip()
-        if len(stripped) > 0:
-            requirements.append(stripped)
-
+requirements = [
+    "napari-plugin-engine >= 0.1.4",
+    "tifffile"
+]
 
 # https://github.com/pypa/setuptools_scm
 use_scm = {"write_to": "napari_brainreg/_version.py"}
@@ -36,6 +31,17 @@ setup(
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "coverage",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+        ]
+    },
     use_scm_version=use_scm,
     setup_requires=["setuptools_scm"],
     classifiers=[
